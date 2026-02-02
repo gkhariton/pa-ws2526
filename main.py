e5e6c13c144d7472480e04b0c4250b86e570490f
gkhariton <grantkhariton@gmail.com>
2026-02-02 17:47:57 +0100

    1c

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import numpy as np
 import pandas as pd
 
 from functions import functions as fn
-
+from functions import generate_group_name
 
 def main():
     file_path = "data\data_GdD_WiSe2526.h5"
-    controllers = ["ARIMA", "DTWS", "PID"]
+    controllers = ["ARIMA", "DTW", "PID"]
     topologies = ["Coupled", "Decentral", "Central"]
     disruptions = ["BlockageConstant", "BlockageCosine", "PumpOutage", "NoDisruption"]
+
+    group_names = generate_group_name(controllers, topologies, disruptions)
+    considered_groups = ["DTW_Coupled_NoDisruption"]
     pass
 
 
